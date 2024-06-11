118cf2feb11c0327cc8f51e15f75c479070e9fc9
Baruch (Ben) Speiser <sbaruch@cambium.co.il>
2024-06-11 11:24:40 +0300

    Minor adjustment to constant visibility in Md5HashService

--- src/main/java/io/cambium/crypto/service/impl/Md5HashService.java
+++ src/main/java/io/cambium/crypto/service/impl/Md5HashService.java
@@ -3,7 +3,7 @@ package io.cambium.crypto.service.impl;
 import io.cambium.crypto.service.HashService;
 
 public class Md5HashService extends BaseHashService implements HashService {
-  static final String HASHING_ALGORITHM = "MD5";
+  private static final String HASHING_ALGORITHM = "MD5";
 
   public Md5HashService() {
     super(HASHING_ALGORITHM);
